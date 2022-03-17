acfdacbd697cb3b8a4868e406307cfec3690f738
Joshua Perez <joshuaetperez@gmail.com>
2022-03-17 16:24:41 -0700

    Updated ResultPrompt.js to make the result more obvious

--- src/components/ResultPrompt.js
+++ src/components/ResultPrompt.js
@@ -2,18 +2,22 @@ import React from 'react';
 import DifficultyToggle from './DifficultyToggle';
 
 function ResultPrompt(props) {
+  let resultText;
   let promptText;
   if (props.hasWonGame) {
+    resultText = 'YOU WON!!!';
     promptText =
-      'You won! You clicked on all unique cards! Click on the button below to start a new game.';
+      'You clicked on all unique cards! Click on the button below to start a new game.';
   } else {
+    resultText = 'YOU LOST!';
     promptText =
-      'You lost! You chose a card that you have previously clicked on! Click on the button below to start a new game.';
+      'You chose that card already! Click on the button below to start a new game.';
   }
 
   return (
     <div className="prompt-container">
       <div className="prompt">
+        <div className="result-text">{resultText}</div>
         <div className="prompt-text">{promptText}</div>
         <DifficultyToggle
           difficulty={props.difficulty}
